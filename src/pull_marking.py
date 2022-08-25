d895800e57f30f9db89b102241ccb38ec8b208b4
Joshua Wright <joshuawright008@gmail.com>
2022-08-25 16:59:38 +1000

    Initial Commit to Fork

--- src/pull_marking.py
+++ src/pull_marking.py
@@ -1,11 +1,28 @@
 #!/usr/bin/env python3
 
+
+from fileinput import close
+from heapq import merge
 import json
 import os
-
+import requests
+from pygments import highlight
+from pygments.lexers import get_lexer_for_filename
+from pygments.formatters import get_formatter_by_name
+import pdfkit
 from canvasapi import Canvas
+from PIL import Image
+from docx2pdf import convert
+from PyPDF2 import PdfMerger
+
+
 
 CONFIG_FILE = "config.json"
+CONFIG_DATA = json.load(open(CONFIG_FILE))
+WORKING_DIR = "temp/"
+DOWNLOAD_DIR = "downloads/"
+ERROR = "error"
+
 
 def load_course():
     '''Load the configuration from file and create a session connecting to the relevant course.
@@ -13,10 +30,7 @@ def load_course():
     Returns the course object.
     '''
 
-    # load config
-    with open("config.json") as f:
-        config_data = json.load(f)
-    api_key = config_data["API_KEY"]
+    api_key = CONFIG_DATA["API_KEY"]
 
     # use environment variable api key if present -- takes precedent over config file
     try:
@@ -25,8 +39,8 @@ def load_course():
         pass
 
     # connect to canvas and retrieve course
-    canvas = Canvas(config_data["API_URL"], api_key)
-    course = canvas.get_course(config_data["COURSE_ID"])
+    canvas = Canvas(CONFIG_DATA["API_URL"], api_key)
+    course = canvas.get_course(CONFIG_DATA["COURSE_ID"])
     return course
 
 
@@ -42,14 +56,104 @@ def get_all_valid_submissions(course):
     valid_submissions = {}
     for a in course.get_assignments():
         print("- pulling submissions for", a.name)
+        # response = requests.get(a.submissions_download_url)
+        # subfile = open("submissions" + str(a.id) + ".zip", "wb").write(response.content)
+        # close()
+        # wget.download(a.submissions_download_url)
         submissions = [s for s in a.get_submissions() 
             if s.submitted_at is not None # something was submitted
             and s.submitted_at_date <= a.due_at_date # it's not overdue
             and (s.graded_at is None or s.graded_at_date < s.submitted_at_date)] # it hasn't ever been graded, or the grade there is for an older submission
         if len(submissions) > 0:
             valid_submissions[a] = submissions
+            for s in submissions:
+                user = course.get_user(s.user_id)
+                compile_pdf(s, user, a.name)
+                # for a in s.attachments:
+                #     print(a['filename'])
+                #     response = requests.get(a['url'])
+                #     subfile = open(a['display_name'], "wb").write(response.content)
+                #     close()
     
     return valid_submissions
+
+
+def compile_pdf(submission, student, task_name):
+    path = WORKING_DIR + student.login_id + "/"
+    if not (os.path.exists(path)):
+        os.makedirs(path)
+    merger = PdfMerger()
+    for a in submission.attachments:
+        filename = ""
+        if validate_file_type(a["display_name"]):
+            filename = convert_to_pdf(a, path)
+        else:
+            print("Invalid filetype: " + a["display_name"])
+            filename = ERROR
+        if (filename != ERROR):
+            merger.append(filename)
+    if not (os.path.exists(DOWNLOAD_DIR)):
+        os.makedirs(DOWNLOAD_DIR)
+    merger.write(DOWNLOAD_DIR + task_name + "_" + student.login_id + "_" + student.name + ".pdf")
+    merger.close
+
+def get_extension(filename):
+    name_split = str.split(filename, ".")
+    extension = name_split[len(name_split) - 1].lower()
+    return extension
+
+
+def validate_file_type(filename):
+    extension = get_extension(filename)
+    for e in CONFIG_DATA["VALID_EXTENSTIONS"]:
+        if extension == e: return True
+    return False
+
+
+def convert_to_pdf(attachment, path):
+    response = requests.get(attachment["url"])
+    filename = path + attachment["display_name"]
+    open(filename, "wb").write(response.content)
+    close()
+    extension = get_extension(attachment["display_name"])
+    match extension:
+        case "cs":
+            filename = code_to_pdf(filename)
+        case "png" | "jpeg" | "jpg" | "bmp":
+            filename = img_to_pdf(filename)
+        case "docx":
+            filename = docx_to_pdf(filename)
+        case "pdf":
+            pass
+        case _:
+            print("Failed to convert unrecognised extension: " + extension)
+            filename = ERROR
+    return filename
+
+
+def code_to_pdf(filename):
+    with (open(filename, "r")) as f:
+        html = open(filename + ".html", "w")
+        code = ""
+        for line in f:
+            code += line
+        html.write(highlight(code, get_lexer_for_filename(filename), get_formatter_by_name("html", full=True, linenos=True, nobackground=True, title=filename)))
+        close()
+    pdfkit.from_file(filename + ".html", filename + ".pdf")
+    return filename + ".pdf"
+
+
+def img_to_pdf(filename):
+    image = Image.open(r"" + filename)
+    pdf = image.convert("RGB")
+    pdf.save(r"" + filename + ".pdf")
+    return filename + ".pdf"
+
+
+def docx_to_pdf(filename):
+    convert(filename, filename + ".pdf")
+    return filename + ".pdf"
+
 
 
 def speedgrader_link(course, assignment, submission):
@@ -79,7 +183,7 @@ def generate_csv(course, valid_submissions):
     student_details = {} # cache student details to avoid unnecessary requests; {userid: ("name, student_id, lab")}
 
     with open("marking.csv", "w+") as f:
-        f.write("Task, Student, Student ID, Lab, Submitted on, Submission number, Link\n")
+        f.write("Task, Student, Student ID, Lab, Submitted on, Times assessed, Link\n")
         for assignment, submissions in valid_submissions.items():
             print("- adding entries for", assignment.name)
             for s in submissions:
